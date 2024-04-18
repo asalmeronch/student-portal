04238f15fef8f928124b8b66c5ada902154b9678
asalmeronch <andreasuhan15@gmail.com>
2024-04-17 18:33:17 -0700

    login updated 3

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,7 @@ export function Header() {
     <header>
       <nav>
         <a href="#">Home</a> | <a href="#">Link</a>
+        <a href="#">Signup</a> | <a href="#">Login</a> | <a href="#">LogoutLink</a>
       </nav>
     </header>
   );
