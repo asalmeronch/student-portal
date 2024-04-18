c0574a22ddba7c7227d595d8395fe20a1a82983c
asalmeronch <andreasuhan15@gmail.com>
2024-04-17 18:22:55 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
-import { Signup } from "./Signup";
-import { Login } from "./Login";
-import { LogoutLink } from "./LogoutLink";
 
 function App() {
   return (
